07b66d981e2215b2982a3af5dcd2f83e8a6c9a13
Mansurov Vladislav <reymax.vlad@gmail.com>
2023-02-16 20:51:10 +0300

    rk-1 add report

--- rk-1/main.cpp
+++ rk-1/main.cpp
@@ -6,7 +6,6 @@
 // структура РСФ - разреженный строчный формат матрицы
 struct matrix_csr_t
 {
-    //matrix_csr_t(int n, int m, int cnt): an(cnt), ja(cnt), nr(n + 1) {};
     std::vector<int> an; // элементы
     std::vector<int> ja; // номер соотвествующего столбца для элемента
     std::vector<int> nr; // номер элемента в an начала следующей строки - размер N + 1, N - количество строк
@@ -73,20 +72,16 @@ void print_mtr_csr(matrix_csr_t &a) {
 int main() {
     setbuf(stdout, nullptr);
     matrix_csr_t mtr;
-
     mtr.n = get_number("Введите количество строк n (больше 0): ", 0, 1000);
     mtr.m = get_number("Введите количество столбцов m (больше 0): ", 0, 1000);
     read_mtr_csr_t(mtr);
     print_mtr_csr(mtr);
-
     if (mtr.an.empty()) {
         printf("Матрица полностью состоит из нулей.\n");
         return 0;
     }
-
-
     int find = 0, f_i = 0;
-    int c = get_number("Введите индекс элемента (больше 0): ", 0, mtr.an.size()) - 1;
+    int c = get_number("Введите индекс элемента (больше 0): ", 1, mtr.an.size()) - 1;
     for (int i = 0; !find && i < mtr.nr.size() - 1; i ++) {
         printf("В %d строке %d элемента(ов). NR[%d] - NR[%d] = %d", i + 1, mtr.nr[i + 1] - mtr.nr[i], i + 2, i + 1,
                mtr.nr[i + 1] - mtr.nr[i]);
@@ -98,14 +93,9 @@ int main() {
                 printf("\tAN[%d] = %d находится в %d строке\n", c + 1, mtr.an[c], i + 1);
                 find = 1;
                 f_i = i;
-            } else {
+            } else
                 printf("\tAN[%d] = %d нет в %d строке -> переходим к следующей строке.\n", c + 1, mtr.an[c], i + 1);
-            }
         }
-//        printf("\nНажмите пробел для следующего шага.");
-//        char ch = 1;
-//        while (ch != '\n')
-//            scanf("%c", &ch);
     }
     printf("Строчной координатой элемента AN[%d] равна %d.\n", c + 1, f_i + 1);
     return 0;
